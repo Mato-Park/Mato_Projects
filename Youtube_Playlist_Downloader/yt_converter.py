171e1b0e4f76bd2891dd802ef4fc187137153b2c
Mato-Park <dark1432@naver.com>
2022-12-13 22:32:09 +0900

    fix class and add new class searcher

--- Youtube_Playlist_Downloader/yt_converter.py
+++ Youtube_Playlist_Downloader/yt_converter.py
@@ -14,6 +14,11 @@ from urllib import request
 
 target_url = ''
 
+"""
+    MainWindow 클래스에 화면 구성을 설정하고
+    MainContent 클래스에 안에 내용을 채우는 식으로 구조를 변경함
+"""
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -74,11 +79,89 @@ class MainWindow(QMainWindow):
 
         wid.setLayout(vbox)
 
+
+class MainContent(MainWindow):
+    add_sec_signal = pyqtSignal()
+    send_instance_signal = pyqtSignal('PyQt_PyObject')
+
+    def __init__(self):
+        super().__init__()
+
+        self.search_button.clicked.connect(self.search)
+
+        self.th_search = searcher(parent = self)
+        self.th_search.updated_list.connect(self.list_update)
+        self.th_search.updated_label.connect(self.status_update)
+
         self.show()
 
+    @pyqtSignal()
+    def search(self):
+        # self.video_list.insertItem(0, 'test')
+        self.video_list.clear()
+        self.th_search.start
+
+    @pyqtSignal(str)
+    def list_update(self, msg):
+        self.video_list.addItem(msg)
+        self.video_list.selectAll()
+
+    @pyqtSignal(str)
+    def status_update(self, msg):
+        self.status_label.setText(msg)
+
+class searcher(QThread):
+    updated_list = pyqtSignal(str)
+    updated_label = pyqtSignal(str)
+    
+    def __init__(self, parent = None):
+        super().__init__()
+        self.main = parent
+
+    def __del__(self): # 소멸자 생성
+        self.wait()
+
+    def run(self):
+
+        global target_url
+        global down_url_list
+        global down_title_list
+
+        down_url_list = []
+        down_title_list = []
+
+        if target_url != "":
+            self.updated_label.emit("Reading Playlist lists...")
+
+            ydl_opts = {'format': 'bestaudio/best'}
+
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(target_url, download = False)
+
+                if 'entries' in info_dict:
+                    video = info_dict['entries']
+
+                    for i, _ in enumerate(video):
+                        video = info_dict['entries'][i]
+                        self.updated_list.emit(info_dict['entries'][i]['title'])
+
+                        down_url_list.append(info_dict['entries'][i]['webpage_url'])
+                        down_title_list.append(info_dict['entries'][i]['title'])
+                else:
+                    video_title = info_dict.get('title', None)
+                    self.updated_list.emit(video_title)
+
+                    down_url_list.append(target_url)
+                    down_title_list.append(video_title)
+            
+            self.updated_label.emit('Load Complete!')
         
+        else:
+            self.updated_label.emit('Please Insert Playlist url')
+
+
 
 app = QApplication(sys.argv)
-window = MainWindow()
+window = MainContent()
 window.show()
 app.exec()
