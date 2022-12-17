0e8e22e9f23ac2e8ff34ffb60d36f410137cf4ff
Mato-Park <dark1432@naver.com>
2022-12-17 22:04:51 +0900

    make search & download class

--- Youtube_Playlist_Downloader/yt_converter.py
+++ Youtube_Playlist_Downloader/yt_converter.py
@@ -12,6 +12,8 @@ from io import BytesIO
 from PIL import Image
 from urllib import request
 import time
+import shutil
+import glob
 
 target_url = ''
 
@@ -71,13 +73,15 @@ class MainWindow(QMainWindow):
         hbox4 = QHBoxLayout()
         hbox4.addWidget(self.download_button)
 
+        self.status_label = QLabel("", self)
+
         vbox = QVBoxLayout()
         # vbox.addStretch(1)
         vbox.addLayout(hbox)
         vbox.addLayout(hbox2)
         vbox.addLayout(hbox3)
         vbox.addLayout(hbox4)
-
+        vbox.addWidget(self.status_label)
         wid.setLayout(vbox)
 
 
@@ -101,26 +105,19 @@ class MainContent(MainWindow):
     def __init__(self):
         super().__init__()
 
-        self.search_button.clicked.connect(self.search)
+        self.search_button.clicked.connect(self.search) # 검색 버튼 불르면 search 매소드를 호출하고 searcher 클래스 실행!
+        self.download_button.clicked.connect(self.download)
 
-        self.search_url.textChanged[str].connect(self.title_update)
+        self.search_url.textChanged[str].connect(self.title_update) # 플레이리스트 url입력하면 그것을 가져오는 매소드 호출
 
-        # self.test = test()
-        # # self.test.start()
-        # self.test.proc.connect(self.proc_)
+        self.th_search = searcher(parent = self) # searcher 클래스 호출하기
+        self.th_search.updated_list.connect(self.list_update) # 시그널 보내기!
+        self.th_search.updated_label.connect(self.status_update)
 
-        self.th_search = searcher(parent = self)
-        self.th_search.updated_list.connect(self.list_update)
-        # self.th_search.updated_label.connect(self.status_update)
+        self.th_download = downloader(parent = self)
+        self.th_download.updated_label.connect(self.status_update)
 
         self.show()
-
-    # def start(self):
-    #     self.test.start()
-
-    # @pyqtSlot(str)
-    # def proc_(self, count):
-    #     self.video_list.insertItem(0, count)
 
     def title_update(self, input):
         global target_url
@@ -131,19 +128,24 @@ class MainContent(MainWindow):
     def search(self):
         # self.video_list.insertItem(0, 'test')
         self.video_list.clear()
-        self.th_search.start
+        self.th_search.start()
+
+    def download(self):
+        self.th_download.start()
 
     @pyqtSlot(str)
     def list_update(self, msg):
-        # self.video_list.addItem(msg)
-        self.video_list.insertItem(0, msg)
+        self.video_list.addItem(msg)
+        # self.video_list.insertItem(0, msg)
         self.video_list.selectAll()
 
-    # @pyqtSignal(str)
-    # def status_update(self, msg):
-    #     self.status_label.setText(msg)
+    @pyqtSlot(str)
+    def status_update(self, msg):
+        self.status_label.setText(msg)
 
-
+"""
+썸네일 다운받는 것도 추가함
+"""
 
 class searcher(QThread):
     updated_list = pyqtSignal(str)
@@ -157,13 +159,15 @@ class searcher(QThread):
         self.wait()
 
     def run(self):
-
+        print("Success!!!!!!")
         global target_url
         global down_url_list
         global down_title_list
+        global down_image_list
         
         down_url_list = []
         down_title_list = []
+        down_image_list = []
         
         if target_url != "":
             self.updated_label.emit("Reading Playlist lists...")
@@ -182,6 +186,7 @@ class searcher(QThread):
 
                         down_url_list.append(info_dict['entries'][i]['webpage_url'])
                         down_title_list.append(info_dict['entries'][i]['title'])
+                        down_image_list.append(info_dict['entries'][i]['thumbnail'])
                         
                 else:
                     video_title = info_dict.get('title', None)
@@ -194,7 +199,58 @@ class searcher(QThread):
         
         else:
             self.updated_label.emit('Please Insert Playlist url')
+
+class downloader(QThread):
+    updated_label = pyqtSignal(str)
+
+    def __init__(self, parent = None):
+        super().__init__()
+        self.main = parent
+    
+    def __del__(self):
+        self.wait()
+    
+    """
+    원작자는 플레이리스트의 비디오들의 타이틀을 가져온 다음 선택한 것만 다운로드 하는 것으로 기능을 구현했지만,
+    난 다운받고 싶은 것만 저장시킨 것이므로 타이틀을 확인후 바로 다운로드 하는 것으로 기능을 구현함
+    그리고 난 영상을 다운받을 필요 없으므로 음원만 다운받는 것만 구현하고 끝!
+    썸네일은 다운받을 순 있지만 쓸만한 게 있을까? 굳이 없어도 될듯 한데 일단 기능을 살려놓자
+    """
+
+    def run(self):
+        global down_url_list
+        global down_title_list
+        global down_image_list
+        # global selected_title
+
+        # titles = []
+
+        cnt = 0
+
+        for i, url in enumerate(down_url_list):
+            self.updated_label.emit("{}/{} Downloading video files...".format(i, len(down_title_list)))
+
+            video_output_dir = os.path.join('./download/', '%(title)s.%(ext)s')
+            # image_output_dir = os.path.join('./image/', '%(title)s.%(ext)s')
+
+            ydl_opt_audio = {
+                'outtmpl': video_output_dir,
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+
+            with yt_dlp.YoutubeDL(ydl_opt_audio) as ydl:
+                ydl.download([url])
+
+            image_url = down_image_list[i]
+            urllib.request.urlretrieve(image_url, '/image/{}_picture.jpg'.format(i))
+
         
+        self.updated_label.emit("Download Complete")
 
 
 app = QApplication(sys.argv)
