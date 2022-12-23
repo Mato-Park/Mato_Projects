23ee4e4e0aaeb3ffb727f36b3ad3870a0bde9c65
Mato-Park <dark1432@naver.com>
2022-12-23 17:15:57 +0900

    add edit mp3 files properties function

--- Youtube_Playlist_Downloader/edit_main.py
+++ Youtube_Playlist_Downloader/edit_main.py
@@ -4,6 +4,10 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 import eyed3
+from eyed3 import id3
+from eyed3 import load
+import urllib
+from urllib import request
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -56,6 +60,7 @@ class MainContent(MainWindow):
     def __init__(self):
         super().__init__()
 
+        global path
         path = '/Users/mato/Desktop/Study/Python/Youtube_Playlists_Downloader/download'
         files = os.listdir(path)
         self.file_list.addItems(files)
@@ -82,8 +87,8 @@ class EditDialog(QDialog):
     def __init__(self):
         super().__init__()
 
-        dialogleft = 1200
-        dialogTop = 120
+        dialogleft = 700
+        dialogTop = 200
         dialogWidth = 600
         dialogHeight = 450
         self.setGeometry(dialogleft, dialogTop, dialogWidth, dialogHeight)
@@ -101,6 +106,8 @@ class EditDialog(QDialog):
         self.file_name = QLabel(edit_file)
         self.button1 = QPushButton('Edit', self)
 
+        self.button1.clicked.connect(self.editbuttonClicked)
+
         hbox1 = QHBoxLayout()
         hbox1.addWidget(self.label1)
         hbox1.addWidget(self.file_name)
@@ -113,9 +120,13 @@ class EditDialog(QDialog):
         hbox2 = QHBoxLayout()
         hbox2.addWidget(self.label2)
 
+        file = path + '/' + edit_file
+        tag = id3.Tag()
+        tag.parse(file)
+
         ### Artist
         self.artist_label = QLabel('Artist: ')
-        self.artist_edit = QLineEdit()
+        self.artist_edit = QLineEdit(tag.artist)
 
         hbox3 = QHBoxLayout()
         hbox3.addWidget(self.artist_label)
@@ -123,7 +134,7 @@ class EditDialog(QDialog):
 
         ### Title
         self.title_label = QLabel('Title: ')
-        self.title_edit = QLineEdit()
+        self.title_edit = QLineEdit(tag.title)
 
         hbox4 = QHBoxLayout()
         hbox4.addWidget(self.title_label)
@@ -131,7 +142,7 @@ class EditDialog(QDialog):
 
         ### Album
         self.album_label = QLabel('Album: ')
-        self.album_edit = QLineEdit()
+        self.album_edit = QLineEdit(tag.album)
 
         hbox5 = QHBoxLayout()
         hbox5.addWidget(self.album_label)
@@ -139,7 +150,12 @@ class EditDialog(QDialog):
 
         ### Genre
         self.genre_label = QLabel('Genre: ')
-        self.genre_edit = QLineEdit()
+        # self.genre_edit = QLineEdit(tag.genre.name)
+
+        if tag.genre == None:
+            self.genre_edit = QLineEdit()
+        else:
+            self.genre_edit = QLineEdit(tag.genre.name)
 
         hbox6 = QHBoxLayout()
         hbox6.addWidget(self.genre_label)
@@ -156,28 +172,37 @@ class EditDialog(QDialog):
         self.image_button = QPushButton('가져오기', self)
         self.image_save_button = QPushButton('저장하기', self)
 
-        self.image = QLabel('None' , self)
-        self.image.resize(100, 100)
+        self.image_button.clicked.connect(self.image_get_button)
+        self.image_save_button.clicked.connect(self.image_save_clicked)
+
+        self.image = QLabel( self)
 
         hbox8 = QHBoxLayout()
-        hbox8_v1 = QVBoxLayout()
-        hbox8_v1_h1 = QHBoxLayout()
-        hbox8_v1_h2 = QHBoxLayout()
-        hbox8_v2 = QVBoxLayout()
-        hbox8_v1_h1.addWidget(self.image_url)
-        hbox8_v1_h1.addWidget(self.image_edit)
-        hbox8_v1_h2.addWidget(self.image_button)
-        hbox8_v1_h2.addWidget(self.image_save_button)
-        hbox8_v1.addLayout(hbox8_v1_h1)
-        hbox8_v1.addLayout(hbox8_v1_h2)
-        hbox8_v2.addWidget(self.image)
-        hbox8.addLayout(hbox8_v1)
-        hbox8.addLayout(hbox8_v2)
+        hbox8.addWidget(self.image_url)
+        hbox8.addWidget(self.image_edit)
+        hbox8.addWidget(self.image_button)
+        hbox8.addWidget(self.image_save_button)
+        hbox8_2 = QHBoxLayout()
+        hbox8_2.addWidget(self.image)
+        # hbox8_v1 = QVBoxLayout()
+        # hbox8_v1_h1 = QHBoxLayout()
+        # hbox8_v1_h2 = QHBoxLayout()
+        # hbox8_v2 = QVBoxLayout()
+        # hbox8_v1_h1.addWidget(self.image_url)
+        # hbox8_v1_h1.addWidget(self.image_edit)
+        # hbox8_v1_h2.addWidget(self.image_button)
+        # hbox8_v1_h2.addWidget(self.image_save_button)
+        # hbox8_v1.addLayout(hbox8_v1_h1)
+        # hbox8_v1.addLayout(hbox8_v1_h2)
+        # hbox8_v2.addWidget(self.image)
+        # hbox8.addLayout(hbox8_v1)
+        # hbox8.addLayout(hbox8_v2)
 
         ### 저장 & 나가기 버튼 생성
         self.save_button = QPushButton('Save', self)
         self.exit_button = QPushButton('Exit', self)
 
+        self.save_button.clicked.connect(self.save_button_clicked)
         self.exit_button.clicked.connect(self.exit_button_clicked)
 
         hbox9 = QHBoxLayout()
@@ -193,12 +218,91 @@ class EditDialog(QDialog):
         vbox.addLayout(hbox6)
         vbox.addLayout(hbox7)
         vbox.addLayout(hbox8)
+        vbox.addLayout(hbox8_2)
         vbox.addLayout(hbox9)
 
         wid.setLayout(vbox)
-    
+
+    def save_button_clicked(self):
+        tag = eyed3.load(path + '/' + edit_file)
+        tag.tag.artist = self.artist_edit.text()
+        tag.tag.title = self.title_edit.text()
+        tag.tag.album = self.album_edit.text()
+        tag.tag.genre = self.genre_edit.text()
+        tag.tag.images.set(3, image_web, "image/jpeg", u"None")
+
+        tag.tag.save()
+
+    def image_get_button(self):
+        image_path = self.image_edit.text()
+        global image_web
+        image_web = urllib.request.urlopen(image_path).read()
+
+        self.qpixel = QPixmap()
+        self.qpixel.loadFromData(image_web)
+        self.image.setPixmap(self.qpixel)
+        self.image.resize(self.qpixel.width(), self.qpixel.height())
+        self.image.repaint()
+        self.image.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.update()
+
+    def image_save_clicked(self):
+        image_path = self.image_edit.text()
+        save_path = '/Users/mato/Desktop/Study/Python/Youtube_Playlists_Downloader/image/' + self.artist_edit.text() + '_' + self.album_edit.text()
+        urllib.request.urlretrieve(image_path, save_path + '.jpg')
+
     def exit_button_clicked(self):
         self.close()
+
+    def editbuttonClicked(self):
+        name_edit = Name_edit()
+        name_edit.exec()
+
+class Name_edit(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        dialogleft = 800
+        dialogTop = 500
+        dialogWidth = 600
+        dialogHeight = 80
+        self.setGeometry(dialogleft, dialogTop, dialogWidth, dialogHeight)
+        self.setWindowTitle('Change File Name')
+        self.setModal(True)
+
+        self.setupUI()
+    
+    def setupUI(self):
+        wid = QWidget(self)
+
+        self.label1 = QLabel('파일명 :')
+        self.name_edit = QLineEdit(edit_file)
+        self.name_edit.setMinimumWidth(500)
+
+        self.change_button = QPushButton('Change!')
+        self.change_button.clicked.connect(self.name_change_clicked)
+
+        hbox1 = QHBoxLayout()
+        hbox2 = QHBoxLayout()
+        
+        hbox1.addWidget(self.label1)
+        hbox1.addWidget(self.name_edit)
+        hbox2.addWidget(self.change_button)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+
+        wid.setLayout(vbox)
+    
+    def name_change_clicked(self):
+        path = '/Users/mato/Desktop/Study/Python/Youtube_Playlists_Downloader/download/'
+        new_name = self.name_edit.text()
+        print(new_name)
+        os.rename(path + edit_file, path + new_name)
+        self.close()
+
+
 
 app = QApplication(sys.argv)
 window = MainContent()
